bb9394e9c2df64cd763a0fab63a1427e36c335ac
cvw <cvw@energisk.eu>
2019-10-12 13:41:58 +0200

    Add ex15 about exception handling

---
+++ springboot-ex15-exceptionhandling/src/main/java/dk/lundogbendsen/springboot/ex/springbootex15exceptionhandling/SpringbootEx15ExceptionhandlingApplication.java
@@ -0,0 +1,14 @@
+package dk.lundogbendsen.springboot.ex.springbootex15exceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEx15ExceptionhandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEx15ExceptionhandlingApplication.class, args);
+    }
+
+
+}
