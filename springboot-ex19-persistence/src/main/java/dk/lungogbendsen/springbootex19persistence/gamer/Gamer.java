dd2f4e465201d1258a6a61a652e937c7da1445c1
cvw <cvwjensen@gmail.com>
2020-11-22 13:26:53 +0100

    Use Lombok @Data

--- springboot-ex19-persistence/src/main/java/dk/lungogbendsen/springbootex19persistence/gamer/Gamer.java
+++ springboot-ex19-persistence/src/main/java/dk/lungogbendsen/springbootex19persistence/gamer/Gamer.java
@@ -1,38 +1,17 @@
 package dk.lungogbendsen.springbootex19persistence.gamer;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Gamer {
     @Id
     @GeneratedValue
     private Long id;
     private String alias;
     private String avatar;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
 }
