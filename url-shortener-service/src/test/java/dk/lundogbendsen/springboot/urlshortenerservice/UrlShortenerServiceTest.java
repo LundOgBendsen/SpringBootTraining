353a4b13802ac1746ea4f419971621c74ca48c7a
cvw <cvw@energisk.eu>
2020-11-24 08:45:12 +0100

    Add Url Shortener Service

---
+++ url-shortener-service/src/test/java/dk/lundogbendsen/springboot/urlshortenerservice/UrlShortenerServiceTest.java
@@ -0,0 +1,41 @@
+package dk.lundogbendsen.springboot.urlshortenerservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UrlShortenerServiceTest {
+
+    private UrlShortenerService urlShortenerService = new UrlShortenerService();
+
+    @Test
+    public void testShortenUrl() {
+        String abc = urlShortenerService.shorten("abc", "http://dr.dk");
+        assertEquals("abc", abc);
+    }
+
+    @Test
+    public void testResolveToken() {
+        String abc = urlShortenerService.shorten("abc", "http://dr.dk");
+        assertEquals("abc", abc);
+
+        String url = urlShortenerService.getTargetUrl(abc);
+        assertEquals("http://dr.dk", url);
+
+    }
+
+    @Test
+    public void testUpdateTokenGivesException() {
+        String abc = urlShortenerService.shorten("abc", "http://dr.dk");
+        assertEquals("abc", abc);
+
+
+        try {
+            abc = urlShortenerService.shorten("abc", "http://example.com");
+            fail("Code should have failed");
+        } catch (Exception e) {
+            assertTrue(e instanceof TokenAlreadyExistsException);
+        }
+
+    }
+}
